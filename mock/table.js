3aa261bf409f331c6d3a58b484a4ac56bf66fbbd
Ji ZHANG <zhangji87@gmail.com>
2022-04-15 07:52:58 +0800

    fix mock

--- mock/table.js
+++ mock/table.js
@@ -1,71 +1,77 @@
-module.exports = {
-  'GET /api/table/list': (req, res) => {
-    let tables = [{
+function getTableList(req, res) {
+  let tables = [{
+    id: 1,
+    sourceInstance: 2,
+    sourceDatabase: 'zhuanqian',
+    sourceTable: 'user',
+    targetInstance: 1,
+    targetDatabase: 'dw_stage',
+    targetTable: 'ds_user',
+    columnList: 'id,create_day',
+    createdAt: '2018-08-18 15:53:01',
+    updatedAt: '2018-08-18 15:53:01',
+  }]
+
+  res.json({
+    payload: {
+      tables,
+    },
+  })
+}
+
+function saveTable(req, res) {
+  res.json({
+    code: 200,
+    payload: {
       id: 1,
-      sourceInstance: 2,
-      sourceDatabase: 'zhuanqian',
-      sourceTable: 'user',
-      targetInstance: 1,
-      targetDatabase: 'dw_stage',
-      targetTable: 'ds_user',
-      columnList: 'id,create_day',
-      createdAt: '2018-08-18 15:53:01',
-      updatedAt: '2018-08-18 15:53:01',
-    }]
+    },
+  })
+}
 
-    res.json({
-      payload: {
-        tables,
-      },
-    })
-  },
+function getTable(req, res) {
+  let table = {
+    id: 1,
+    sourceInstance: 2,
+    sourceDatabase: 'zhuanqian',
+    sourceTable: 'user',
+    targetInstance: 1,
+    targetDatabase: 'dw_stage',
+    targetTable: 'ds_user',
+    columnList: 'id,create_day',
+  }
 
-  'POST /api/table/save': (req, res) => {
-    res.json({
-      code: 200,
-      payload: {
-        id: 1,
-      },
-    })
-  },
+  let columns = ['id', 'nickname', 'create_day']
 
-  'GET /api/table/get': (req, res) => {
-    let table = {
-      id: 1,
-      sourceInstance: 2,
-      sourceDatabase: 'zhuanqian',
-      sourceTable: 'user',
-      targetInstance: 1,
-      targetDatabase: 'dw_stage',
-      targetTable: 'ds_user',
-      columnList: 'id,create_day',
+  res.json({
+    payload: {
+      table,
+      columns,
     }
+  })
+}
 
-    let columns = ['id', 'nickname', 'create_day']
+function getTableColumns(req, res) {
+  let columns = ['id', 'nickname', 'create_day']
 
-    res.json({
-      payload: {
-        table,
-        columns,
-      }
-    })
-  },
+  res.json({
+    payload: {
+      columns,
+    }
+  })
+}
 
-  'GET /api/table/columns': (req, res) => {
-    let columns = ['id', 'nickname', 'create_day']
+function deleteTable(req, res) {
+  res.json({
+    payload: {
+      message: 'ok',
+    },
+  })
+}
 
-    res.json({
-      payload: {
-        columns,
-      }
-    })
-  },
-
-  'POST /api/table/delete': (req, res) => {
-    res.json({
-      payload: {
-        message: 'ok',
-      },
-    })
-  },
+module.exports = {
+  'GET /api/table/list': getTableList,
+  'POST /api/table/save': saveTable,
+  'GET /api/table/get': getTable,
+  'GET /api/table/columns': getTableColumns,
+  'POST /api/table/delete': deleteTable,
 }
