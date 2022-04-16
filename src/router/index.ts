0d9f131f1da446d676e4b9190efd7d58618bc67e
Ji ZHANG <zhangji87@gmail.com>
2022-04-16 16:30:06 +0800

    basic layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,23 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import TableList from '@/views/table/List.vue'
+import Monitor from '@/views/Monitor.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: HomeView,
+      path: '/table/list',
+      component: TableList,
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      path: '/monitor',
+      component: Monitor,
+    },
+    {
+      path: '/',
+      redirect: '/table/list',
     },
   ],
-});
+})
 
-export default router;
+export default router
---
+++ src/views/table/List.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>table list</div>
+</template>
