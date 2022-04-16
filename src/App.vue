19d2b335ef67af7c482f15ebaf669329137abdde
Ji ZHANG <zhangji87@gmail.com>
2022-04-16 13:15:37 +0800

    vue3

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,125 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
 <template>
-  <div id="app">
-    <el-container style="height: 100vh;">
-      <el-header class="app-header">
-        <img src="./assets/logo.png">Morph
-      </el-header>
-      <el-container>
-        <el-aside width="240px">
-          <el-menu :router="true" style="height: 100%;">
-            <el-menu-item index="/table/list">
-              <i class="el-icon-sort"></i>
-              <span>ETL</span>
-            </el-menu-item>
-            <el-menu-item index="/monitor">
-              <i class="el-icon-bell"></i>
-              <span>Monitor</span>
-            </el-menu-item>
-          </el-menu>
-        </el-aside>
-        <el-main style="padding-top: 15px;">
-          <router-view/>
-        </el-main>
-      </el-container>
-    </el-container>
-  </div>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
 
-<style lang="scss">
-body {
-  margin: 0;
-  padding: 0;
-}
+<style>
+@import "@/assets/base.css";
 
 #app {
-  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
-  font-size: 14px;
-  color: #606266;
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
 }
 
-.app-header {
-  font-size: 20px;
-  font-weight: bold;
-  line-height: 60px;
-  color: #303133;
-  box-shadow: 0 2px 8px #f0f1f2;
-  margin-bottom: 10px;
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
 
-  img {
-    height: 40px;
-    margin-right: 20px;
-    vertical-align: middle;
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
   }
 }
 </style>
