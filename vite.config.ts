296430d03f09cc0168c836aef00ee745290f74cb
Ji ZHANG <zhangji87@gmail.com>
2022-05-09 15:59:07 +0800

    Fix mocks

--- vite.config.ts
+++ vite.config.ts
@@ -26,11 +26,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8081',
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://127.0.0.1:8081',
+  //     },
+  //   },
+  // },
 })
